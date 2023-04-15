5a46f6e260c4cd1f3b65fd0b9a0be8e2c95beebf
hsu <viewsonic521@gmail.com>
2023-04-15 12:47:53 +0800

    move _app index api into src and delete useless styles

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '../chakra/theme';
+import Layout from '../components/Layout/Layout';
+
+function MyApp({ Component, pageProps }: AppProps) {
+	return (
+		<ChakraProvider theme={theme}>
+			<Layout />
+			<Component {...pageProps} />
+		</ChakraProvider>
+	);
+}
+
+export default MyApp;
