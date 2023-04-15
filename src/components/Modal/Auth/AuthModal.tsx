b7ae5d4750fa39f4626d24241b8fa1a610a80939
hsu <viewsonic521@gmail.com>
2023-04-15 16:57:54 +0800

    create authinput components

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -9,6 +9,7 @@ import {
 } from '@chakra-ui/react';
 import { useRecoilState } from 'recoil';
 import { authModalState } from '../../../atoms/authModalAtom';
+import AuthInputs from './AuthInputs';
 
 const AuthModal: React.FC = () => {
 	const [modalState, setModalState] = useRecoilState(authModalState);
@@ -24,16 +25,17 @@ const AuthModal: React.FC = () => {
 			<Modal isOpen={modalState.open} onClose={handleClose}>
 				<ModalOverlay />
 				<ModalContent>
-					<ModalHeader>
+					<ModalHeader textAlign='center'>
 						{modalState.view === 'login' && 'Login'}
 						{modalState.view === 'signup' && 'Sign Up'}
 						{modalState.view === 'resetPassword' && 'Reset Password'}
 					</ModalHeader>
 					<ModalCloseButton />
-					<ModalBody display='flex' flexDirection='column' alignItems='center' justifyContent='center'>
-						<Flex direction='column' align='center' justify='center' width='70%' border='1px solid red'>
+					<ModalBody display='flex' flexDirection='column' alignItems='center' justifyContent='center' pb={6}>
+						<Flex direction='column' align='center' justify='center' width='70%'>
+							<AuthInputs />
 							{/* <OAuthButtons
-                                <AuthInputs />
+
                             <ResetPassword /> */}
 						</Flex>
 					</ModalBody>
