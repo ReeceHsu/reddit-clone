88d2d5b0aa498bc2818a8a6f6e16f74d39ac3295
hsu <viewsonic521@gmail.com>
2023-04-15 12:53:49 +0800

    add button styles, searchinput and authbutton in rightcontent component

---
+++ src/components/RightContent/AuthButtons.tsx
@@ -0,0 +1,23 @@
+import { Button } from '@chakra-ui/react';
+
+type TypeName = {};
+
+const AuthButtons: React.FC<TypeName> = () => {
+	return (
+		<>
+			<Button
+				variant='outline'
+				height='28px'
+				display={{ base: 'none', sm: 'flex' }}
+				width={{ base: '70px', md: '110px' }}
+				mr={2}>
+				Log In
+			</Button>
+			<Button height='28px' display={{ base: 'none', sm: 'flex' }} width={{ base: '70px', md: '110px' }} mr={2}>
+				Sign Up
+			</Button>
+		</>
+	);
+};
+
+export default AuthButtons;
