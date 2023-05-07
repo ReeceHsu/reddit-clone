ef2b81bbcf646c15648afa4914acdad5e6cf94cc
hsu <viewsonic521@gmail.com>
2023-05-07 12:47:26 +0800

    create newPostForm component

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -1,4 +1,4 @@
-import { Flex, Icon } from '@chakra-ui/react';
+import { Alert, AlertIcon, Flex, Icon, Text } from '@chakra-ui/react';
 import React from 'react';
 import { BiPoll } from 'react-icons/bi';
 import { BsLink45Deg, BsMic } from 'react-icons/bs';
@@ -6,8 +6,16 @@ import { IoDocumentText, IoImageOutline } from 'react-icons/io5';
 import TabItem from './TabItem';
 import TextInput from './PostForm/TextInput';
 import ImageUpload from './PostForm/ImageUpload';
+import { Post } from '../../atoms/post';
+import { User } from 'firebase/auth';
+import { useRouter } from 'next/router';
+import { Timestamp, addDoc, collection, serverTimestamp, updateDoc } from 'firebase/firestore';
+import { firestore, storage } from '../../firebase/clientApp';
+import { getDownloadURL, ref, uploadString } from 'firebase/storage';
 
-type NewPostFormProps = {};
+type NewPostFormProps = {
+	user: User;
+};
 
 const formTabs = [
 	{
@@ -37,7 +45,8 @@ export type TabItem = {
 	icon: typeof Icon.arguments;
 };
 
-const NewPostForm: React.FC<NewPostFormProps> = () => {
+const NewPostForm: React.FC<NewPostFormProps> = ({ user }) => {
+	const router = useRouter();
 	const [selectedTab, setSelectedTab] = React.useState(formTabs[0].title);
 	const [textInputs, setTextInputs] = React.useState({
 		title: '',
@@ -45,8 +54,49 @@ const NewPostForm: React.FC<NewPostFormProps> = () => {
 	});
 	const [selectedFile, setSelectedFile] = React.useState('');
 	const [loading, setLoading] = React.useState(false);
+	const [error, setError] = React.useState(false);
 
-	const handleCreatePost = async () => {};
+	const handleCreatePost = async () => {
+		const { communityId } = router.query;
+
+		// create new post
+		const newPost: Post = {
+			communityId: communityId as string,
+			creatorId: user.uid,
+			creatorDisplayName: user.email!.split('@')[0],
+			title: textInputs.title,
+			body: textInputs.body,
+			numberOfComments: 0,
+			voteStatus: 0,
+			createdAt: serverTimestamp() as Timestamp,
+		};
+
+		setLoading(true);
+		try {
+			// store the post in db
+			const postDocRef = await addDoc(collection(firestore, 'posts'), newPost);
+
+			// check for selectedFile
+			if (selectedFile) {
+				// store in storage => getDownloadURL (return imageURL)
+				const imageRef = ref(storage, `posts/${postDocRef.id}/image`);
+				await uploadString(imageRef, selectedFile, 'data_url');
+				const downloadURL = await getDownloadURL(imageRef);
+
+				// update post doc by adding imageURL
+				await updateDoc(postDocRef, {
+					imageURL: downloadURL,
+				});
+			}
+		} catch (error) {
+			console.log('handleCreatePost error', error);
+			setError(true);
+		}
+		setLoading(false);
+
+		// redirect the user back to the communityPage using the router
+		router.back();
+	};
 
 	const onSelectImage = (event: React.ChangeEvent<HTMLInputElement>) => {
 		const reader = new FileReader();
@@ -93,6 +143,12 @@ const NewPostForm: React.FC<NewPostFormProps> = () => {
 						setSelectedTab={setSelectedTab}
 						setSelectedFile={setSelectedFile}
 					/>
+				)}
+				{error && (
+					<Alert status='error'>
+						<AlertIcon />
+						<Text mr={2}>Error creating post</Text>
+					</Alert>
 				)}
 			</Flex>
 		</Flex>
--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import PageContent from '../../../components/Layout/PageContent';
 import { Box, Text } from '@chakra-ui/react';
 import NewPostForm from '../../../components/Posts/NewPostForm';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../../../firebase/clientApp';
 
 const SubmitPostPage: React.FC = () => {
+	const [user] = useAuthState(auth);
 	return (
 		<>
 			<PageContent>
@@ -11,7 +14,7 @@ const SubmitPostPage: React.FC = () => {
 					<Box p='14px 0px' borderBottom='1px solid' borderColor='white'>
 						<Text>Create a post</Text>
 					</Box>
-					<NewPostForm />
+					{user && <NewPostForm user={user} />}
 				</>
 				<>
 					<div>RHS</div>
